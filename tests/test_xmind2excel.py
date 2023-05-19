60ae43b777536a449fb74d2288a6ccc640dbdc6d
韩志超 <han_zhichao@sina.cn>
2023-05-19 21:10:34 +0800

    fix priority

---
+++ tests/test_xmind2excel.py
@@ -0,0 +1,5 @@
+from xmind2excel import xmind2excel
+
+
+def test_xmind2excel():
+    xmind2excel("./testcases.xmind", "./testcases.xlsx", owner='韩志超')
--- xmind2excel/__init__.py
+++ xmind2excel/__init__.py
@@ -5,71 +5,88 @@ import openpyxl
 from openpyxl.styles import PatternFill
 from xmindparser import xmind_to_dict
 
-OWNER = '韩志超'
+DEFAULT_OWNER = ''
+DEFAULT_TITLE_LINE = ('模块', '验证功能', '验证场景', '用例标题', '优先级', '预置条件', '测试步骤', '期望结果', '归属人')
 
-data = xmind_to_dict('../tests/testcases.xmind')
+
+def load_xmind(xmind_file: str, sheet_index=0) -> dict:
+    data = xmind_to_dict(xmind_file)
+    pprint(data)
+    return data[sheet_index]
+
+
 # pprint(data)
+def parse_testcases(xmind_data: dict, owner=DEFAULT_OWNER):
+    root_topic = xmind_data['topic']
+    testcases = []
 
-root_topic = data[0]['topic']
-testcases = []
+    for module in root_topic['topics']:
+        module_title = module['title']
 
-for module in root_topic['topics']:
-    module_title = module['title']
+        for feature in module.get('topics', []):
+            feature_title = feature['title']
 
-    for feature in module.get('topics', []):
-        feature_title = feature['title']
+            for scenario in feature.get('topics', []):
+                scenario_title = scenario['title']
 
-        for scenario in feature.get('topics', []):
-            scenario_title = scenario['title']
+                for testcase in scenario.get('topics', []):
+                    testcase_title = testcase['title']
+                    priority = ''
+                    precondition = ''
+                    owner = owner
 
-            for testcase in scenario.get('topics', []):
-                testcase_title = testcase['title']
-                priority = ''
-                precondition = ''
-                owner = OWNER
+                    steps = []
+                    excepted = []
 
-                steps = []
-                excepted = []
+                    note = testcase.get('note')
+                    if note:
+                        precondition = note  # todo 解析 [预置条件]
 
-                note = testcase.get('note')
-                if note:
-                    precondition = note  # todo 解析 [预置条件]
+                    for index, step in enumerate(testcase.get('topics', [])):
+                        step_sn = index + 1
+                        step_title = step['title']
+                        if 'topics' in step.keys():
+                            step_excepted = ' \n'.join([item['title'] for item in step['topics']])
+                            excepted.append(f'{step_sn}. {step_excepted}')
 
-                for index, step in enumerate(testcase.get('topics', [])):
-                    step_sn = index + 1
-                    step_title = step['title']
-                    step_excepted = None
-                    if 'topics' in step.keys():
-                        step_excepted = ' \n'.join([item['title'] for item in step['topics']])
-                        excepted.append(f'{step_sn}. {step_excepted}')
+                        steps.append(f'{step_sn}. {step_title}')
 
-                    steps.append(f'{step_sn}. {step_title}')
+                    makers = testcase.get('makers')
+                    print('markers', makers)
+                    if makers:
+                        priority_markers = [item for item in makers if 'priority' in item]
+                        if priority_markers:
+                            priority = priority_markers[0].replace('priority-', 'p')
 
-                markers = testcase.get('markers')
-                if markers:
-                    priority_markers = [item.startwith('priority') for item in markers]
-                    if priority_markers:
-                        priority = priority_markers[0]
-
-
-
-                test_steps = ' \n'.join(steps)
-                test_excepted = ' \n'.join(excepted)
-                testcase_data = (module_title, feature_title, scenario_title, testcase_title, precondition, test_steps, test_excepted, owner)
-                testcases.append(testcase_data)
-                print('测试用例', testcase_data)
+                    test_steps = ' \n'.join(steps)
+                    test_excepted = ' \n'.join(excepted)
+                    testcase_data = (
+                        module_title, feature_title, scenario_title, testcase_title, priority, precondition, test_steps,
+                        test_excepted, owner)
+                    testcases.append(testcase_data)
+                    print('测试用例', testcase_data)
+    return testcases
 
 
-excel = openpyxl.Workbook()
-sheet = excel.active
-title_line = ['模块', '验证功能', '验证场景', '用例标题', '预置条件', '测试步骤', '期望结果', '归属人']
-sheet.append(title_line)
+def write_excel(testcases, output_excel_file: str = 'case.xlsx', title_line: list = DEFAULT_TITLE_LINE):
+    excel = openpyxl.Workbook()
+    sheet = excel.active
+    title_line = title_line
+    sheet.append(title_line)
 
-color = PatternFill('solid', fgColor="FF00FF")
-for i in range(1, 9):
-    sheet.cell(1, i).fill = color
+    color = PatternFill('solid', fgColor="FF00FF")
+    for i in range(1, len(title_line)+1):
+        sheet.cell(1, i).fill = color
 
-for testcase in testcases:
-    sheet.append(testcase)
+    for testcase in testcases:
+        sheet.append(testcase)
 
-excel.save("case.xlsx")
+    excel.save(output_excel_file)
+
+
+def xmind2excel(xmind_file, excel_file=None, owner=None):
+    xmind_data = load_xmind(xmind_file)
+    testcases = parse_testcases(xmind_data, owner=owner)
+    write_excel(testcases, excel_file)
+
+
